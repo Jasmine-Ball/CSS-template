4eb8cd45d7d35805b8fee7c9dbf691af2712aef7
Jasmine-Ball <jasmine.ball@uon.edu.au>
2021-04-09 16:13:48 +1000

    Add Grid.js file for main JSX.

---
+++ Grid.js
@@ -0,0 +1,25 @@
+
+function Grid() {
+    return (
+    <>
+    <div class="grid_container">
+        <div class="Left_Top">LT</div>
+        <div class="Top_Bar_L">
+            <div class="Bar_Items">test</div>
+        </div>
+        <div class="Top_Bar_R">
+            <div class="Bar_Items">test</div>
+        </div>
+        <div class="Right_Top">RT</div> 
+        <div class="Body">
+            <div class="Panel"><p>Div Within Body</p>  </div>
+        </div>
+        <div class="Body_Low">Body Low</div>
+        <div class="Footer">Footer</div>
+    </div>
+    </>
+             )
+
+}
+
+export default Grid
